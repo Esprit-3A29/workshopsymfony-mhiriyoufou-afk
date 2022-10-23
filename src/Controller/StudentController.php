04995dffbe322d92b73c36df35ec8799b24d4169
Youssef Mhiri <youssef.mhiri@esprit.tn>
2022-10-23 14:05:21 +0100

    test commit

---
+++ src/Controller/StudentController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class StudentController extends AbstractController
+{
+    #[Route('/student', name: 'app_student')]
+    public function index(): Response
+    {
+        return $this->render('student/index.html.twig', [
+            'controller_name' => 'StudentController',
+        ]);
+    }
+
+    #[Route('/index',name:'funtionindex')]
+    public function etudiant()
+    {
+        return new Response("Bonjour mes etudiants"); 
+    }
+}
